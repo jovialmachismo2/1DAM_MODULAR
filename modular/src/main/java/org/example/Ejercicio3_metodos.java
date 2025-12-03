faa80920bb3a743f1c87e32bf7b1c66b64b30e0b
A704\dam1 <joaquinmagnoli10@gmail.com>
2025-12-03 21:17:32 +0100

    ejercicios por hacer

--- modular/src/main/java/org/example/Ejercicio3_metodos.java
+++ modular/src/main/java/org/example/Ejercicio3_metodos.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Ejercicio3_metodos {
     static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribir texto en minuscula");
+        String palabra = teclado.nextLine();
 
+        palabra = imprimir(palabra);
+
+        System.out.println(imprimir(palabra));;
+
+        System.out.println(vocales(palabra));
+    }
+    public static String imprimir(String x){
+
+        return x.toUpperCase();
+
+    }
+    public static int vocales(String palabra) {
+        int num_vocales = 0;
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.charAt(i) == 'A' ||palabra.charAt(i) == 'E' ||palabra.charAt(i) == 'I' ||palabra.charAt(i) == 'O' ||palabra.charAt(i) == 'U'){
+                num_vocales++;
+            }
+        }
+        return num_vocales;
     }
 }
---
+++ modular/src/main/java/org/example/Ejercicio4_metodos.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicio4_metodos {
+    static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        for (int i = 0; i < 7; i++) {
+
+        }
+
+    }
+    static int (){
+
+    }
+}
