7a2a7fe9af3afe13a8725515b6bf991836be843d
A704\dam1 <joaquinmagnoli10@gmail.com>
2025-12-10 21:15:36 +0100

    hormiga comilona

--- modular/src/main/java/org/example/Projecto_aceptaelreto.java
+++ modular/src/main/java/org/example/Projecto_aceptaelreto.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Projecto_aceptaelreto {
+    static Scanner teclado ;
+
+    static void main(String[] args) {
+        teclado = new Scanner(System.in);
+    int corteza = 1;
+
+
+        while (corteza != 0){
+
+            corteza = teclado.nextInt();
+            String trozitos = teclado.nextLine();
+            String bocadillo[] = trozitos.split(" ");
+            if (bocadillo.length != corteza){
+                return;
+            }
+
+        }
+    }
 }
