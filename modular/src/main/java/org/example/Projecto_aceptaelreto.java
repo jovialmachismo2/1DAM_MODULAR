a9b1afc27caf968e6dc5df0c27d577175a425163
A704\dam1 <joaquinmagnoli10@gmail.com>
2025-12-12 19:25:33 +0100

    ya se hacerlo o eso creo

--- modular/src/main/java/org/example/Projecto_aceptaelreto.java
+++ modular/src/main/java/org/example/Projecto_aceptaelreto.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Projecto_aceptaelreto {
@@ -8,17 +9,27 @@ public class Projecto_aceptaelreto {
     static void main(String[] args) {
         teclado = new Scanner(System.in);
     int corteza = 1;
-
+    int suma = 0;
 
         while (corteza != 0){
-
             corteza = teclado.nextInt();
-            String trozitos = teclado.nextLine();
-            String bocadillo[] = trozitos.split(" ");
-            if (bocadillo.length != corteza){
-                return;
+            teclado.nextLine();
+            if (corteza != 0) {
+                String bocadillos[] = teclado.nextLine().split(" ");
+                int pasar_int_bocadillo[] = new int[bocadillos.length];
+                if (bocadillos.length != corteza) {
+                    return;
+                }else {
+                    for (int i = 0; i < bocadillos.length; i++) {
+                        pasar_int_bocadillo[i] = Integer.parseInt(bocadillos[i]);
+                        suma = pasar_int_bocadillo[i] + suma;
+                    } 
+                }
             }
-
         }
+
+    }
+    static void modo2(int corteza){
+        System.out.println(corteza);
     }
 }
